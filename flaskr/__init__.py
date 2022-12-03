9b43573c893c88441c6fe808b0670c9dff84c7fd
yangls06 <yangls06@126.com>
2022-12-03 23:08:18 +0800

    add blog blueprint and corresponding views and templates

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -44,4 +44,9 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    # add blog blueprint
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
